9e93552e85443ce3bee94943b6b11251b14c0901
warinratt-debug <warinrat.t@ku.th>
2026-02-13 12:38:53 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,84 @@
+<<<<<<< HEAD
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 8000;
+
+let users = [];
+let counter = 1;
+/**
+ *GET /users - ดึงข้อมูลผู้ใช้ท้ังหมด
+ POST /users - เพิ่มผู้ใช้ใหม่
+ GET /users/:id - ดึงข้อมูลผู้ใช้ตาม ID
+ PUT /users/:id -แก้ไขข้อมูลผู้ใช้ตาม ID ที่บันทึก
+ DELETE /users/:id - ลบผู้ใช้ตาม ID ที่บันทึก
+ */
+
+// path: = GET / users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path: = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+
+    users.push(user);
+    res.json({
+    message: 'User added successfully',
+    user: user
+    });
+});
+
+// path: = PUT /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updateUser = req.body;
+
+    // หา user ทีจาก id ที่ส่งมา
+    let selectedIndex = users.findIndex(user => user.id == id);
+
+    // อัพเดทข้อมูล users
+    if (updateUser.fname) {
+        users[selectedIndex].fname = updateUser.fname;
+    }
+    if (updateUser.lname) {
+        users[selectedIndex].lname = updateUser.lname;
+    }
+
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: updateUser,
+            indexUpdate: selectedIndex
+        }
+    });
+    // ส่ง users ที่อัพเดทแล้วกลับไป
+})
+
+app.delete('/users/:id', (req, res) => {
+    let id = req.params.id;
+    // หา index จาก id ที่ต้องการลบ
+      let selectedIndex = users.findIndex(user => user.id == id);
+
+    // ลบ user ออกจาก users
+    users.splice(selectedIndex, 1);
+
+    res.json({
+        message: 'User deleted successfully',
+        indexDelete: selectedIndex
+    });
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+=======
 // ทำการ import โมดูล http
 const http = require('http');
 const host = 'localhost';
@@ -13,4 +94,5 @@ const requireListener = function (req, res){
 const server = http.createServer(requireListener);
 server.listen(port, host, () => {
     console.log(`server is running on http://${host}:${port}`);
+>>>>>>> 577266d1a2409a1a7fe1f9420bacf6cf907734dd
 });
