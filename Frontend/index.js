fef59524dcd26b21a9393903a6a2bc04ef24802a
warinratt-debug <warinrat.t@ku.th>
2026-03-13 11:46:07 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -21,7 +21,6 @@ const validateData = (userData) => {
     return errors;
 }
 
-
 const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
@@ -30,44 +29,48 @@ const submitData = async () => {
     let interestDOMs = document.querySelectorAll('input[name=interests]:checked') || {};
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
-    let messageDom = document.getElementById('message')
+    let messageDOM = document.getElementById('message')
     try {
-    let interest = ''
+        let interest = ''
         for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+            interest += interestDOMs[i].value
+            if (i != interestDOMs.length - 1) {
+                interest += ','
+            }
         }
-    }
 
-    let userData ={
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interests: interest
-    }
-    console.log('submitData', userData);
-
-    const errors = validateData(userData);
-    if (errors.length > 0){
-        throw {
-            message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
-            errors: errors
+        let userData = {
+            firstName: firstNameDOM.value,
+            lastName: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
         }
-    }
+        console.log('submitData', userData);
+
+        const errors = validateData(userData);
+        if (errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
 
         const response = await axios.post('http://localhost:8000/users', userData);
-        console.log('response',response);
-        messageDom.innerText = 'บันทึกข้อมูลสำเร็จ';
-        messageDom.className = 'message success';
-    }catch (error) {
-        console.log('error message',error.message);
-        console.log('error details',error.error);
-        //if (error.response) {
-       //     console.error('Error response:', error.response.data.message);
-       // }
+        console.log('response', response);
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success';
+    } catch (error) {
+        console.log('error message', error.message);
+        console.log('error', error.errors);
+
+        if (error.response) {
+            console.log('Error response:', error.response);
+            error.message = error.response.data.message
+            error.errors = error.response.data.errors
+        }
+
        let htmlData = '<div>'
        htmlData += `<div>${error.message}</div>`;
        htmlData += '<ul>';
